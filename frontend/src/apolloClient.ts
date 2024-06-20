df255e80f951673ece58994f61de44f5f71add48
Thiha Htet Zaw <thihahtetzaw7991@gmail.com>
2024-06-20 20:47:11 +0630

    add user store

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -8,3 +8,24 @@ import {
     split,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { useUserStore } from "../stores/userStore";
+
+async function refreshToken(client: ApolloClient<NormalizedCacheObject>){
+    try {
+        const { data } = await client.mutate({
+            mutation: gql`
+                mutation RefreshToken {
+                    refreshToken
+                }
+            `,
+        })
+        const newAccessToken = data?.refreshToken
+        if(!newAccessToken){
+            throw new Error("New access token not received.")
+        }
+    }catch(err){
+        throw new Error("Error getting new access token.")
+    }
+}
